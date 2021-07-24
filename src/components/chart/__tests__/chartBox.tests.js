820fd356b71e9197ce58b5e3425adc34eeae17ae
Kacper <hernackikacper@gmail.com>
2021-07-24 15:57:37 +0200

    chart basics

--- src/components/chart/__tests__/chartBox.tests.js
+++ src/components/chart/__tests__/chartBox.tests.js
@@ -3,14 +3,14 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../../app/store';
 import '@testing-library/jest-dom/extend-expect';
-import Chart from '../Chart';
+import ChartBox from '../ChartBox';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 test('render chart', async () => {
   const { getByText } = render(
     <Provider store={store}>
       <Router>
-        <Chart />
+        <ChartBox />
       </Router>
     </Provider>
   );
--- src/pages/coinData/CoinData.js
+++ src/pages/coinData/CoinData.js
@@ -3,7 +3,7 @@ import './CoinData.css';
 import { useParams } from 'react-router-dom';
 import { selectCoins } from '../../features/coinsSlice';
 import { useSelector } from 'react-redux';
-import Chart from '../../components/chart/Chart';
+import ChartBox from '../../components/chart/ChartBox';
 
 function CoinData() {
   const { coinId } = useParams();
@@ -19,37 +19,107 @@ function CoinData() {
       <h1 data-testid="coin-name">{coinId}</h1>
       <div className="coinData__basics">
         <img src={result?.image} alt="" />
-        <p>${result?.current_price}</p>
-        <p>${result?.market_cap}</p>
-        <p>{result?.market_cap_rank}</p>
-        <p>${result?.total_volume}</p>
+        <div className="coinData__basicsData">
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Price:</span> $
+            {result?.current_price}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Market cap:</span> $
+            {result?.market_cap}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Market cap ranking: </span>{' '}
+            {result?.market_cap_rank}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Total Volume: </span> $
+            {result?.total_volume}
+          </p>
+        </div>
       </div>
-      <div className="coinData__priceFlow">
-        <h2>Price change</h2>
-        <p>${result?.high_24h}</p>
-        <p>${result?.low_24h}</p>
-        <p>${result?.price_change_24h}</p>
-        <p>{result?.price_change_percentage_24h}%</p>
-        <p>${result?.market_cap_change_24h}</p>
-        <p>{result?.market_cap_change_percentage_24h}%</p>
-        <p>${result?.circulating_supply}</p>
+      {/*++++++++++++++++++++++++++++++Chart++++++++++++++++++++++++++++++*/}
+      <ChartBox />
+      {/*++++++++++++++++++++++++++++++Chart++++++++++++++++++++++++++++++*/}
+      <div className="coinData__priceData">
+        <div className="coinData__priceFlow">
+          <h2>Price change</h2>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Day Max Price:</span>$
+            {result?.high_24h}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Day Min Price:</span>$
+            {result?.low_24h}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Day Price Change: </span>$
+            {result?.price_change_24h}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">
+              Day Price Change Percentage:
+            </span>
+            {result?.price_change_percentage_24h}%
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Day Market Cap Change:</span>$
+            {result?.market_cap_change_24h}
+          </p>
+          <p className="coinData__textRow">
+            {' '}
+            <span className="coinData__attribute">
+              Day Market Cap Change Percentage:
+            </span>
+            {result?.market_cap_change_percentage_24h}%
+          </p>
+          <p className="coinData__textRow">
+            {' '}
+            <span className="coinData__attribute">Circulating Supply:</span>$
+            {result?.circulating_supply}
+          </p>
+        </div>
+        <div className="coinData__priceFlow">
+          <h2>ATH</h2>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATH:</span>${result?.ath}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATH Change Percentage:</span>
+            {result?.ath_change_percentage}%
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATH Date</span>
+            {result?.ath_date}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATL:</span>${result?.atl}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATL Change Percentage::</span>
+            {result?.atl_change_percentage}%
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ATL Date</span>
+            {result?.atl_date}
+          </p>
+        </div>
+        <div className="coinData__priceFlow">
+          <h2>ROI</h2>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ROI times:</span>
+            {result?.roi?.times}
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">ROI Percentage:</span>
+            {result?.roi?.percentage}%
+          </p>
+          <p className="coinData__textRow">
+            <span className="coinData__attribute">Last Update:</span>
+            {result?.last_updated}
+          </p>
+        </div>
       </div>
-      <div className="coinData__priceFlow">
-        <h2>ATH</h2>
-        <p>${result?.ath}</p>
-        <p>{result?.ath_change_percentage}%</p>
-        <p>{result?.ath_date}</p>
-        <p>${result?.atl}</p>
-        <p>{result?.atl_change_percentage}%</p>
-        <p>{result?.atl_date}</p>
-      </div>
-      <div className="coinData__priceFlow">
-        <h2>ROI</h2>
-        <p>{result?.roi?.times}</p>
-        <p>{result?.roi?.percentage}%</p>
-      </div>
-      <p>{result?.last_updated}</p>
-      <Chart />
     </div>
   );
 }
